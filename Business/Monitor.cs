5b17be3053038935f3259497a11921c2eb62899a
ArtemDamell <artem.damell@gmail.com>
2023-04-16 18:08:09 +0300

    Add project files.

---
+++ Business/Monitor.cs
@@ -0,0 +1,87 @@
+﻿using System.Diagnostics;
+using Models;
+
+namespace Business
+{
+    public class ProcessMonitor
+    {
+        // Interval for checking - 10 seconds
+        private const int CHEACKING_INTERVAL = 10000;
+        private readonly List<ProcessModel> _processesForCilling;
+        private static int _count = 0;
+
+        public ProcessMonitor(List<ProcessModel> processes)
+        {
+            _processesForCilling = processes;
+        }
+
+        /// <summary>
+        /// Start monitoring and killing founded processes from list
+        /// </summary>
+        /// <returns>Nothing, just loop</returns>
+        public async Task StartMonitoring()
+        {
+            Console.WriteLine($"Monitoring started! {DateTime.Now}");
+
+            while (true)
+            {
+                // Get all current running processes
+                var processes = Process.GetProcesses();
+
+                // If processes not found, stop this iteration logic
+                if (processes.Length == 0)
+                    continue;
+
+                // Esle, start process cycle
+                foreach (var currentProcess in processes)
+                {
+                    foreach (var processForKilling in _processesForCilling)
+                    {
+                        if (currentProcess.ProcessName.Contains(processForKilling.ProcessName))
+                        {
+                            KillTheProcess(currentProcess);
+                        }
+                    }
+                }
+                await Task.Delay(CHEACKING_INTERVAL);
+            }
+        }
+
+        /// <summary>
+        /// Kill process method
+        /// </summary>
+        /// <param name="processForKill">Process for killing</param>
+        private static void KillTheProcess(Process processForKill)
+        {
+            try
+            {
+                ProcessStartInfo info = new ProcessStartInfo
+                {
+                    FileName = "CMD.exe",
+                    Arguments = $"/C taskkill /im {processForKill.ProcessName}.exe /f",
+                    CreateNoWindow = true,
+                    UseShellExecute = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    Verb = "runas"
+                };
+
+                Process killProcess = new Process
+                {
+                    StartInfo = info
+                };
+                killProcess.Start();
+                _count++;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Process {processForKill} with ID: {processForKill.Id} killing sucessful! {DateTime.Now}");
+                Console.WriteLine($"Total killed process count: {_count}");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ Models/ProcessModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+namespace Models
+{
+    public class ProcessModel
+    {
+        public string ProcessName { get; set; }
+
+        public ProcessModel(string processName)
+        {
+            ProcessName = processName;
+        }
+
+        /// <summary>
+        /// Check process running right now
+        /// </summary>
+        /// <returns>true/false</returns>
+        public bool IsProcessRunning()
+        {
+            // Try to get all existing processes by name
+            var processes = Process.GetProcessesByName(ProcessName);
+            return processes is null;
+        }
+    }
+}
